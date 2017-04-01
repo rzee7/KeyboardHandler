3cd8d164e726b74b820d3836aea9e826a372a7ff
M RIYAZ <rzee.m7@gmail.com>
2017-04-01 23:49:11 +0530

    Format Code

--- KeyboardHandler/Extensions/ScrollViewExtensions.cs
+++ KeyboardHandler/Extensions/ScrollViewExtensions.cs
@@ -1,10 +1,70 @@
 ﻿using System;
+using Foundation;
+using UIKit;
+
 namespace KeyboardHandler
 {
-	public class ScrollViewExtensions
-	{
-		public ScrollViewExtensions()
-		{
-		}
-	}
+    /// <summary>
+    /// Scroll View and UIView Extensions
+    /// </summary>
+    public static class ScrollViewExtensions
+    {
+        #region Private Fields
+
+        private static readonly KeyboardManager kbHandler;
+        private static NSObject keyboardUp;
+        private static NSObject keyboardDown;
+
+        #endregion
+
+        #region Constructor
+
+        static ScrollViewExtensions()
+        {
+            if (kbHandler == null)
+                kbHandler = new KeyboardManager();
+        }
+
+        #endregion
+
+        #region Subscribe  View Keyboard Handling
+
+        /// <summary>
+        /// Subscribes the keyboard manager.
+        /// </summary>
+        /// <param name="scrollView">Scroll view.</param>
+        /// <param name="extraHeight">Extra height to make field just top of the keyboard, if UITextField is too high/hidden, Default is 70.</param>
+        public static void SubscribeKeyboardManager(this UIScrollView scrollView, float extraHeight = 70)
+        {
+            kbHandler.View = scrollView;
+            kbHandler.ExtraHeight = extraHeight;
+            keyboardUp = NSNotificationCenter
+                .DefaultCenter
+                .AddObserver(UIKeyboard.DidShowNotification,
+                    kbHandler.KeyboardUpNotification);
+            keyboardDown = NSNotificationCenter
+                .DefaultCenter
+                .AddObserver(UIKeyboard.WillHideNotification,
+                    kbHandler.KeyboardDownNotification);
+        }
+
+        #endregion
+
+        #region Unsubscribe View Keyboard Handling
+
+        public static void UnsubscribeKeyboardManager(this UIScrollView scrollView)
+        {
+            if (keyboardUp != null && keyboardDown != null)
+            {
+                NSNotificationCenter
+                    .DefaultCenter
+                    .RemoveObserver(keyboardUp);
+                NSNotificationCenter
+                    .DefaultCenter
+                    .RemoveObserver(keyboardDown);
+            }
+        }
+
+        #endregion
+    }
 }
--- KeyboardHandler/Handler/KeyboardManager.cs
+++ KeyboardHandler/Handler/KeyboardManager.cs
@@ -1,10 +1,71 @@
 ﻿using System;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
 namespace KeyboardHandler
 {
-	public class KeyboardManager
-	{
-		public KeyboardManager()
-		{
-		}
-	}
+    /// <summary>
+    /// Keyboard Manager
+    /// </summary>
+    public class KeyboardManager
+    {
+        #region Private Fields
+
+        private CGSize _keyboardFrame;
+
+        #endregion
+
+        #region Public Properties
+
+        public UIScrollView View { get; set; } // The UIView for the keyboard handler
+        public float ExtraHeight { get; set; }
+
+        #endregion
+
+        #region Keyboard Up Notification
+
+        public void KeyboardUpNotification(NSNotification notification)
+        {
+            //Get the keyboard size UIKeyboardFrameEndUserInfoKey
+            var val = notification.UserInfo.ValueForKey(UIKeyboard.FrameEndUserInfoKey) as NSValue;
+            _keyboardFrame = val.RectangleFValue.Size;
+            SetViewUp(true);
+        }
+
+        #endregion
+
+        #region Keyboard Down Notification
+
+        public void KeyboardDownNotification(NSNotification notification)
+        {
+            SetViewUp();
+        }
+
+        #endregion
+
+        #region Set View Up/Down
+
+        /// <summary>
+        /// Sets the view up.
+        /// </summary>
+        /// <param name="isUp">If set to <c>true</c> is up.</param>
+        internal void SetViewUp(bool isUp = false)
+        {
+            try
+            {
+                if (this.View == null)
+                    return;
+                UIEdgeInsets edgInset = isUp ? new UIEdgeInsets(0.0f, 0.0f, _keyboardFrame.Height + ExtraHeight, 0.0f) : UIEdgeInsets.Zero;
+                this.View.ContentInset = edgInset;
+                this.View.ScrollIndicatorInsets = edgInset;
+            }
+            catch (Exception ex)
+            {
+                //Eating Exception Here
+            }
+        }
+
+        #endregion
+    }
 }
